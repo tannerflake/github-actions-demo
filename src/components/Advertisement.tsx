87738a4f7624e70bb850f1bd05831d6bff539f6c
Tanner Flake <tannerflake@gmail.com>
2025-03-13 20:26:16 -0600

    resolved lint error

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -2,7 +2,7 @@ interface AdvertisementProps {
   companyName: string;
   productDescription: string;
   price: string;
-};
+}
 
 function Advertisement(props: AdvertisementProps) {
   return (
@@ -11,7 +11,7 @@ function Advertisement(props: AdvertisementProps) {
       <p>{props.productDescription}</p>
       <p>{props.price}</p>
     </div>
-  );
+  )
 }
 
 export default Advertisement;
